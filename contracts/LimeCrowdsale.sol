cc014acf9139065496137cae39a080a07098d4fc
Yoskata <root@localhost.localdomain>
2018-05-09 05:43:22 -0400

    Fixed spacings

--- contracts/LimeCrowdsale.sol
+++ contracts/LimeCrowdsale.sol
@@ -4,105 +4,105 @@ import '../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol';
 import './LimeToken.sol';
 
 contract LimeCrowdsale {
-    using SafeMath for uint256;
+        using SafeMath for uint256;
 
-    event TokensBought(address from, address  to, uint256 amount);
+	event TokensBought(address from, address  to, uint256 amount);
 
-    LimeToken token;
+	LimeToken token;
 
-    address public wallet;
-    uint256 public totalWeiRaised;
+	address public wallet;
+	uint256 public totalWeiRaised;
 
-    // start and end timestamps where investments are allowed (both inclusive)
-    uint256 public saleStart;
-    uint256 public saleEnd;
+	// start and end timestamps where investments are allowed (both inclusive)
+	uint256 public saleStart;
+	uint256 public saleEnd;
 
-    // end timestamps for the two phases where rates for the investments are changed (both inclusive)
-    uint256 public firstPhaseEnd;
-    uint256 public secondPhaseEnd;
+	// end timestamps for the two phases where rates for the investments are changed (both inclusive)
+	uint256 public firstPhaseEnd;
+	uint256 public secondPhaseEnd;
 
-    constructor (address _wallet) public {
-	    //contract cannot assume the balance is zero upon creation
-	    require(address(this).balance == 0);
-	    require(_wallet != address(0));
+	constructor (address _wallet) public {
+	        //contract cannot assume the balance is zero upon creation
+		require(address(this).balance == 0);
+		require(_wallet != address(0));
 
-	    wallet = _wallet;
-		
-	    saleStart = now;
-	    saleEnd = saleStart + 30 days;
+		wallet = _wallet;
+			
+		saleStart = now;
+		saleEnd = saleStart + 30 days;
 
-	    firstPhaseEnd = saleStart + 7 days;
-	    secondPhaseEnd = firstPhaseEnd + 7 days;
+		firstPhaseEnd = saleStart + 7 days;
+		secondPhaseEnd = firstPhaseEnd + 7 days;
 
-	    token = new LimeToken(saleEnd);
-    }
+		token = new LimeToken(saleEnd);
+	}
 
-    function () external payable {
-        totalWeiRaised = totalWeiRaised.add(msg.value);
-        _forwardFunds();
-    }
+        function () external payable {
+		totalWeiRaised = totalWeiRaised.add(msg.value);
+		_forwardFunds();
+	}
 
-    modifier beforeSaleEnd() {
-	require(now <= saleEnd);
-	_;
-    }
+        modifier beforeSaleEnd() {
+		require(now <= saleEnd);
+		_;
+	}
 
-    function buyTokens() public payable beforeSaleEnd returns (bool) {
-	    require(msg.value != 0);
+	function buyTokens() public payable beforeSaleEnd returns (bool) {
+		require(msg.value != 0);
 
-	    uint256 tokenAmount = getTokenAmount(msg.value);
+		uint256 tokenAmount = getTokenAmount(msg.value);
 
-	    totalWeiRaised = totalWeiRaised.add(msg.value);
+		totalWeiRaised = totalWeiRaised.add(msg.value);
 
-	    token.transfer(msg.sender, tokenAmount);
+		token.transfer(msg.sender, tokenAmount);
 
-	    emit TokensBought(address(this), msg.sender, msg.value);
+		emit TokensBought(address(this), msg.sender, msg.value);
 
-	    _forwardFunds();
+		_forwardFunds();
 
-	    return true;
-    }
+		return true;
+	}
 
-    function getTokenAmount(uint256 _weiAmount) public view beforeSaleEnd returns(uint256) {
-            uint256 currentPhaseRate = 100;
-	    uint256 nextPhaseRate;
+        function getTokenAmount(uint256 _weiAmount) public view beforeSaleEnd returns(uint256) {
+		uint256 currentPhaseRate = 100;
+		uint256 nextPhaseRate;
 
-	    uint256 currentPhaseWei;
-	    uint256 nextPhaseWei;
+		uint256 currentPhaseWei;
+		uint256 nextPhaseWei;
 
-	    uint256 tokensAmount;
+		uint256 tokensAmount;
 
-	    if(now <= firstPhaseEnd){
-	        phaseEtherCap = 10 ether;
-	        currentPhaseRate = 500;
-	        nextPhaseRate = 300;
-	    } else if (now > firstPhaseEnd && now <= secondPhaseEnd) {
-	        phaseEtherCap = 30 ether;
-	        currentPhaseRate = 200;
-	        nextPhaseRate = 150;
-	    }
+		if(now <= firstPhaseEnd){
+			phaseEtherCap = 10 ether;
+			currentPhaseRate = 500;
+			nextPhaseRate = 300;
+		} else if (now > firstPhaseEnd && now <= secondPhaseEnd) {
+			phaseEtherCap = 30 ether;
+			currentPhaseRate = 200;
+			nextPhaseRate = 150;
+		}
 
-	    if(phaseEtherCap < totalWeiRaised){
-            currentPhaseRate = nextPhaseRate;
-	    }
+		if(phaseEtherCap < totalWeiRaised){
+		        currentPhaseRate = nextPhaseRate;
+		}
 
-            // case when wei amount should be split and converted between two phases using different rate for each one
-	    if(totalWeiRaised < phaseEtherCap && totalWeiRaised + _weiAmount > phaseEtherCap){
-		    nextPhaseWei = (totalWeiRaised +  _weiAmount) - phaseEtherCap;
-		    currentPhaseWei = phaseEtherCap - totalWeiRaised;
-		    tokensAmount = _calculateTokenAmount(currentPhaseWei, currentPhaseRate).add(_calculateTokenAmount(nextPhaseWei, nextPhaseRate));
-	    } else {
-		    tokensAmount = _calculateTokenAmount(_weiAmount, currentPhaseRate);
-	    }
+		// case when wei amount should be split and converted between two phases using different rate for each one
+		if(totalWeiRaised < phaseEtherCap && totalWeiRaised + _weiAmount > phaseEtherCap){
+			nextPhaseWei = (totalWeiRaised +  _weiAmount) - phaseEtherCap;
+			currentPhaseWei = phaseEtherCap - totalWeiRaised;
+			tokensAmount = _calculateTokenAmount(currentPhaseWei, currentPhaseRate).add(_calculateTokenAmount(nextPhaseWei, nextPhaseRate));
+		} else {
+			tokensAmount = _calculateTokenAmount(_weiAmount, currentPhaseRate);
+		}
 
-	    return tokensAmount;
-    }
+		return tokensAmount;
+	}
 
-    function _calculateTokenAmount(uint256 _weiAmount, uint256 _rate) private pure returns (uint256) {
-	    return _weiAmount.mul(_rate);
-    }
+        function _calculateTokenAmount(uint256 _weiAmount, uint256 _rate) private pure returns (uint256) {
+	        return _weiAmount.mul(_rate);
+	}
 
-    function _forwardFunds() internal {
-            wallet.transfer(msg.value);
-    }
+	function _forwardFunds() internal {
+		wallet.transfer(msg.value);
+	}
 }
--- contracts/LimeToken.sol
+++ contracts/LimeToken.sol
@@ -3,32 +3,32 @@ pragma solidity ^0.4.21;
 import '../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 contract LimeToken is MintableToken {
-    string public name = 'LimeChain Exam Token';
-    string public symbol = 'LET';
-    uint8 public constant decimals = 18;
+        string public name = 'LimeChain Exam Token';
+        string public symbol = 'LET';
+        uint8 public constant decimals = 18;
 
-    uint256 public investmentAllowedAfter;
+        uint256 public investmentAllowedAfter;
 
-    modifier onlyIfInvestmentAllowed() {
-            require(now >= investmentAllowedAfter);
-	    _;
-    }
+        modifier onlyIfInvestmentAllowed() {
+                require(now >= investmentAllowedAfter);
+	        _;
+        }
 
-    function transfer(address _to, uint256 _value) public onlyIfInvestmentAllowed returns (bool) {
-	    return super.transfer(_to, _value);
-    }
+        function transfer(address _to, uint256 _value) public onlyIfInvestmentAllowed returns (bool) {
+	        return super.transfer(_to, _value);
+        }
 
-    function transferFrom(address _from, address _to, uint256 _value) public onlyIfInvestmentAllowed returns (bool) {
-	    return super.transferFrom(_from, _to, _value);
-    }
+        function transferFrom(address _from, address _to, uint256 _value) public onlyIfInvestmentAllowed returns (bool) {
+	        return super.transferFrom(_from, _to, _value);
+        }
 
-    constructor (uint256 _investmentAllowedAfter) public {
-	    //contract cannot assume the balance is zero upon creation
-	    require(address(this).balance == 0);
-	    require(mint(msg.sender, 1000000));
+        constructor (uint256 _investmentAllowedAfter) public {
+	        //contract cannot assume the balance is zero upon creation
+	        require(address(this).balance == 0);
+	        require(mint(msg.sender, 1000000));
 
-	    investmentAllowedAfter = _investmentAllowedAfter;
+	        investmentAllowedAfter = _investmentAllowedAfter;
 
-	    emit Mint(msg.sender, 1000000);
-    }
+	        emit Mint(msg.sender, 1000000);
+        }
 }
